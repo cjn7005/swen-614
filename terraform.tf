4de68934d9e42fc37358d515314c6c4184d80169
cjn7005 <cjn7005@rit.edu>
2026-02-11 13:40:24 -0500

    fixed bucket reference

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
-        bucket = "my-terraform-state-bucket"    # S3 bucket for state storage
-        key = "prod/terraform.tfstate"          # State file path in the bucket
+        bucket = "swen614-cjn7005-mybucket"    # S3 bucket for state storage
+        key = "cjn7005/terraform.tfstate"          # State file path in the bucket
         region = "us-east-2"                    # AWS region
         encrypt = true
     }
