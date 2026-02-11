045f7b9cda586d721de7b776c8f7e6ad63f6dfea
cjn7005 <cjn7005@rit.edu>
2026-02-11 13:36:41 -0500

    removed version requirement

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,4 @@
 terraform {
-    required_version = ">= 1.11.0"
-
     backend "s3" {
         bucket = "my-terraform-state-bucket"    # S3 bucket for state storage
         key = "prod/terraform.tfstate"          # State file path in the bucket
