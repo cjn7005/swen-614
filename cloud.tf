cab980370fbc9c1e8177dbd6c28ae407188e6890
cjn7005 <cjn7005@rit.edu>
2026-02-11 13:56:35 -0500

    changed availability zone

--- cloud.tf
+++ cloud.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "wordpress_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.wordpress_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-east-2a"
   map_public_ip_on_launch = true
   tags = {
     Name = "WordPress Public Subnet"
@@ -26,7 +26,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.wordpress_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = "us-east-2b"
   tags = {
     Name = "WordPress Private Subnet"
   }
